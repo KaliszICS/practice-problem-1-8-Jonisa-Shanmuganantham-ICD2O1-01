fe677b31804777acbd773402f7cd6235feee3aa5
Jonisa-Shanmuganantham-ICD2O1-01 <jonisa.shanmuganantham@student.tdsb.on.ca>
2024-09-26 14:19:46 +0000

    a# Please enter the commit message for your changes. Lines starting

--- PP1_8.py
+++ PP1_8.py
@@ -1,12 +1,26 @@
-
+'''
+    Lesson: Boolean Logic
+    Author: Jonisa Shanmuganantham
+    Date Created: Sept 26, 2024
+    Date Last Modified: Sept 26, 2024
+'''
 def q1():
-  #Write Assignment code here
+  bool1 = True
+  bool2 = False
+  print(bool1 and bool2)
+  print(bool1 or bool2)
 
 def q2():
-  #Write Assignment code here
+  integer = input("Enter an integer: ")
+  integer = int(integer)
+  integer = integer not 0
+  print(integer)
 
 def q3():
-  #Write Assignment code here
+  number = input("Enter a number: ")
+  number = int(number)
+  number = number >= 0 and number <= 10
+  print(number)
 
 def q4():
   #Write Assignment code here
