e04e9db0913a295a00c6f7f6534f30f01657b5a2
Jonisa-Shanmuganantham-ICD2O1-01 <jonisa.shanmuganantham@student.tdsb.on.ca>
2024-09-27 14:38:07 +0000

    e#

--- PP1_8.py
+++ PP1_8.py
@@ -32,13 +32,14 @@ def q5():
   integer = input("Input an integer: ")
   num = integer % 2
   bool1 = num == 0
-  print(f"The integer {integer} is {bool1}.")
+  print(f"The integer {integer} is {bool1}.") 
 
 #Do not edit code after this
 #Comment out the followwing code when running tests
-
+'''
 q1()
 q2()
 q3()
 q4()
 q5()
+'''
